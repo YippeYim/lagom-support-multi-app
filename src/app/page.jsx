1150dcef9d035b05994b3886906a8dadbbc1ecab
YippeYim <piya.yim2003@gmail.com>
2026-03-21 05:08:10 +0700

    add irs and user role

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,32 @@
 'use client'
 import { useLogin } from "@/components/auth/useLogin";
 import { useState } from "react";
+import { createClient } from "../../utils/supabase/client";
+
+const supabase = createClient();
 
 export function LoginPage() {
   const [ selectedEmail, setSelectedEmail ] = useState();
+  const [ note , setNote ] = useState([]);
 
   const { user, errorMessage, loginWithEmailPassword} = useLogin();
   const handleLogin = async () => {
-    loginWithEmailPassword(selectedEmail, "12345");
+    await loginWithEmailPassword(selectedEmail, "12345");
+    const { data: profile, error: profileError } = await supabase
+      .from('profiles')
+      .select('*')
+      .single();
+    console.log("Your Role in DB is:", profile?.role);
+  }
+
+  const getTable = async () => {
+    const { data, error } = await supabase
+      .from('notes')
+      .select('*')
+
+    setNote(data);
+    console.log(data);
+    console.log(user?.id);
   }
 
   return (
@@ -22,10 +41,23 @@ export function LoginPage() {
         className={`p-2 rounded border border-white-100 ${selectedEmail === "ggmail@gmail.com" ? 'bg-blue-100' : ''}`}
         onClick={()=>setSelectedEmail("ggmail@gmail.com")}
       >ggmail</button>
+      <button
+        className={`p-2 rounded border border-white-100 ${selectedEmail === "ggmail@gmail.com" ? 'bg-blue-100' : ''}`}
+        onClick={()=>setSelectedEmail("boy@gmail.com")}
+      >boy no sup</button>
+      <button
+        className={`p-2 rounded border border-white-100 ${selectedEmail === "ggmail@gmail.com" ? 'bg-blue-100' : ''}`}
+        onClick={()=>setSelectedEmail("boysupport@gmail.com")}
+      >boy sup</button>
     </div>
 
-    <button onClick={handleLogin}>Sign in</button>      
+    <button onClick={handleLogin}>Sign in</button><br/>
+    <button onClick={getTable}>get</button>      
     <h1>{user?.id}{errorMessage}</h1>
+    
+    {note.map((col=>(
+      <p key={col.id}>{col.title}</p>
+    )))}
     </div>
   );
 }
